11e09dea3c3126a39e8039804464ef17fb9f83f2
Zhixuan Zhang <zhangzx45@gmail.com>
2024-10-20 19:29:51 -0700

    add share link copy and its functionality

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -7,12 +7,14 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { EffectFade, Autoplay, Navigation, Pagination } from "swiper/modules";
 import SwiperCore from "swiper";
 import "swiper/css/bundle";
-import { effectTarget } from "swiper/effect-utils";
+import { FaShare } from "react-icons/fa";
 
 export default function Listing() {
   const params = useParams();
   const [listing, setlisting] = useState(null);
   const [loading, setloading] = useState(true);
+  const [shareLinkCopied, setShareLinkCopied] = useState(false);
+
   SwiperCore.use([Autoplay, Navigation, Pagination]);
 
   useEffect(() => {
@@ -49,12 +51,29 @@ export default function Listing() {
               className="relative w-full overflow-hidden h-[300px]"
               style={{
                 background: `URL(${listing.imgUrls[index]}) center no-repeat`,
-                backgroundSize:"cover",
+                backgroundSize: "cover",
               }}
             ></div>
           </SwiperSlide>
         ))}
       </Swiper>
+      <div
+        className="fixed top-[11%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400
+      rounded-full w-12 h-12 flex justify-center items-center"
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href);
+          setShareLinkCopied(true);
+          setTimeout(() => {
+            setShareLinkCopied(false)
+          }, 2000);
+        }}
+      >
+        <FaShare className="text-lg text-slate-500"></FaShare>
+      </div>
+      {shareLinkCopied && 
+        <p className="fixed top-[17%] right-[5%] font-semibold border-2 border-gray-400
+        rounded-md bg-white z-10">Link copied!</p>
+      }
     </main>
   );
 }
